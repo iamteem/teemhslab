cf7bac1692c56de57c8fae810c035efc67dba5de
Tim Medina <iamteem@gmail.com>
2015-09-30 22:27:25 +0800

    Third try works.

--- hackerrank/sherlock-and-the-beast-ii.hs
+++ hackerrank/sherlock-and-the-beast-ii.hs
@@ -1,10 +1,8 @@
-import Data.List (maximumBy)
-
 greatestDecentNumber :: Integer -> String
-greatestDecentNumber n
-  | null combinations = "-1"
-  | otherwise = displayCombination (maximumBy compareTuples combinations)
-  where combinations = getCombinations n
+greatestDecentNumber n = case c of
+    Just t -> displayCombination t
+    Nothing -> "-1"
+  where c = getCombination n
 
 displayCombination :: (Integer, Integer) -> String
 displayCombination (threes, fives) = (takeN fives (repeat '5')) ++ (takeN threes (repeat '3'))
@@ -12,15 +10,11 @@ displayCombination (threes, fives) = (takeN fives (repeat '5')) ++ (takeN threes
 takeN :: Integer -> [a] -> [a]
 takeN n = take (fromInteger n)
 
-getCombinations :: Integer -> [(Integer, Integer)]
-getCombinations n = [(x, y) | x <- [0..n], y <- [0..n], x + y == n, x `rem` 5 == 0, y `rem` 3 == 0]
-
-compareTuples :: (Integer, Integer) -> (Integer, Integer) -> Ordering
-compareTuples (threes, fives) (threes', fives')
-  | fives > fives' = GT
-  | (fives == fives') && (threes > threes') = GT
-  | (threes == threes') && (fives == fives') = EQ
-  | otherwise = LT
+getCombination :: Integer -> Maybe (Integer, Integer)
+getCombination n
+  | null xs = Nothing
+  | otherwise = Just (head xs)
+  where xs = [(x, y) | x <- [0..n], y <- [n, (n - 1)..0], x + y == n, x `rem` 5 == 0, y `rem` 3 == 0]
 
 sherlock :: String -> String
 sherlock s = let slines = lines s
